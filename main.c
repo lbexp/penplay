2d32ad610f1d18c0c7493c84d9a1046226afcd0c
lbexp <fiqarapr@gmail.com>
2025-04-17 00:46:50 +0700

    feat: init setup libusb

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ApplicationServices/ApplicationServices.h>
+#include <libusb-1.0/libusb.h>
 #include <stdio.h>
 
 /*********************
@@ -38,7 +39,16 @@ void MouseClick(int x, int y) {
 
 int isConnected = 0;
 
-void InitConnection() { isConnected = 1; }
+void InitConnection(libusb_context *ctx, libusb_device_handle *dev) {
+    libusb_init(&ctx);
+    // TODO: Adjust the dummy vid & pid into actual vid & pid
+    dev = libusb_open_device_with_vid_pid(ctx, 0x18D1, 0x4EE7);
+    libusb_claim_interface(dev, 0);
+
+    if (dev) {
+        isConnected = 1;
+    }
+}
 
 void Listen(void (*cb)(char *)) {
     // TODO: Add logic to listen and invokes callback with the correct payload
@@ -60,14 +70,37 @@ void ParseMouseMove(char *payload) {
 
 int main() {
     printf("Initialize connection...");
-    InitConnection();
+
+    libusb_context *ctx = NULL;
+    libusb_device_handle *dev = NULL;
+
+    InitConnection(ctx, dev);
 
     if (isConnected) {
         printf("Connected!");
     }
 
+    unsigned char data[64];
+    int actualLength;
+    int endpoint = 0x81;
+    int r;
+
     while (isConnected) {
         Listen(ParseMouseMove);
+        r = libusb_interrupt_transfer(dev, endpoint, data, sizeof(data),
+                                      &actualLength, 1000);
+
+        if (r == 0 && actualLength > 0) {
+            printf("Received %d bytes: ", actualLength);
+
+            for (int i = 0; i < actualLength; i++) {
+                printf("%02X", data[i]);
+            }
+
+            printf("\n");
+        } else {
+            printf("Failed to received: %s\n", libusb_error_name(r));
+        }
     }
 
     printf("Disconnected.");
